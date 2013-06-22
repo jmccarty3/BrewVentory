d3a7da0361cc0ce3f7c0048fe35a5af3dab3f7e3
Justin McCarty <jmccarty3@gmail.com>
2013-06-22 19:46:51 -0400

    Added the Selected id list. Selection detection is working with template

--- BrewVentory.js
+++ BrewVentory.js
@@ -6,6 +6,8 @@ Inventory = new Meteor.Collection('inventory');
 
 if (Meteor.isClient) {
 
+  Session.setDefault('selectedBeers', []);
+
   Template.inventory.stock = function(){
       return Inventory.find({in_stock: true});
   };
@@ -16,13 +18,49 @@ if (Meteor.isClient) {
       },
   });
 
+  //Helper Function to add/remove from the selection list
+  function selectedBeerIndex(id){
+          var selectedList = Session.get('selectedBeers');
+          console.log("List: ");
+          console.log(selectedList);
+          console.log("Selected List Lenght: " + selectedList.length);
+
+          try{
+              for(var index =0; index < selectedList.length; index++){
+                  if (selectedList[index] == id)
+                      return index;
+
+              }
+          }
+          catch(err){
+              console.log("Error!: ");
+              console.log(err);
+          }
+
+          return -1;
+  };
+
   Template.beer.events({
       "click" : function() {
-          console.log("Clicked a beer");
-          this.selected = !this.selected;
-          this.selected = true;
+          console.log("Clicked a beer: " + this.name );
+          var index = selectedBeerIndex(this._id);
+          var list = Session.get('selectedBeers');
+
+          if(index >= 0){
+            list.splice(index,1);
+          }
+          else{
+              list.push(this._id);
+          }
+          Session.set('selectedBeers', list);
       }
   });
+
+  Template.beer.selected = function(){
+      //console("Selected: " + this.selected);
+      
+      return selectedBeerIndex(this._id) >=0 ? "selected" : '';
+  };
 
   
   Template.newBeer.brewery = function(){
@@ -33,6 +71,8 @@ if (Meteor.isClient) {
       "click input.add": function(){
           console.log("Adding beer")
           console.log(this.name);
+          console.log(this);
+          console.log(this._id);
           //Beers.insert({name: this.name, brewery: "nullz" });
       }
   });
