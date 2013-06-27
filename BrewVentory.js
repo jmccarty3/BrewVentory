1b3cd9a0bde5673c1b642dc2dc7c478530873925
Justin McCarty <jmccarty3@gmail.com>
2013-06-27 16:25:23 -0400

    Selecting Brews works

--- BrewVentory.js
+++ BrewVentory.js
@@ -66,7 +66,7 @@ if (Meteor.isClient) {
   Template.newBeer.events({
       "click input.add": function(){
           console.log("Adding beer")
-          //Beers.insert({name: this.name, brewery: "nullz" });
+          Beers.insert({name: this.name, brewery: "nullz" });
       }
   });
 }
